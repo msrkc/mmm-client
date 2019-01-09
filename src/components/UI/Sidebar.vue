985f7821ece56febf8833220fef412fe28598f7a
Murat S <m@murat.work>
2019-01-09 21:17:17 +0100

    sidebar

--- src/components/UI/Sidebar.vue
+++ src/components/UI/Sidebar.vue
@@ -1,11 +1,26 @@
 <template>
-<div class="sidebar">
-  <div class="burger">
-    <div class="burger-bar"></div>
-    <div class="burger-bar"></div>
-    <div class="burger-bar"></div>
+  <div class="sidebar">
+    <div class="burger">
+      <div class="burger-bar"></div>
+      <div class="burger-bar"></div>
+      <div class="burger-bar"></div>
+    </div>
+    <div class="icons">
+      <div class="icons__item icons-active">
+        <img src="@/assets/img/sidebar/home.svg" alt="">
+      </div>
+      <div class="icons__item">
+        <img src="@/assets/img/sidebar/user.svg" alt="">
+      </div>
+      <div class="icons__item">
+        <img src="@/assets/img/sidebar/tool.svg" alt="">
+      </div>
+      <div class="icons__item">
+        <img src="@/assets/img/sidebar/calendar.svg" alt="">
+      </div>
+    </div>
+
   </div>
-</div>
 </template>
 
 <script>
@@ -16,6 +31,7 @@ export default {
     }
   }
 }
+
 </script>
 
 <style>
