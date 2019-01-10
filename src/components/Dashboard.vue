08ee5cae96283cc2a6bc0277930a53b15dc71d29
Murat S <m@murat.work>
2019-01-10 23:29:44 +0100

    routerLink

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,5 @@
 <template>
 <div>
-  <div style="position:absolute;top:20%;left:7%;font-size:1.2rem">
-    <router-link to="/welcome">welcome page</router-link> <br>
-    <router-link to="/dashboard/information">company information edit page</router-link>
-  </div>
-
 <router-view v-if="isAuth"></router-view>
 </div>
 </template>
--- src/components/UI/Sidebar.vue
+++ src/components/UI/Sidebar.vue
@@ -5,20 +5,20 @@
       <div class="burger-bar"></div>
       <div class="burger-bar"></div>
     </div>
-    <div class="icons">
-      <div class="icons__item icons-active">
-        <img src="@/assets/img/sidebar/home.svg" alt="">
-      </div>
-      <div class="icons__item">
+    <ul class="icons">
+      <router-link to="/dashboard" tag="li" class="icons__item" exact-active-class="icons__item icons-active">
+      <img src="@/assets/img/sidebar/home.svg" alt="">
+      </router-link>
+      <li class="icons__item">
         <img src="@/assets/img/sidebar/user.svg" alt="">
-      </div>
-      <div class="icons__item">
-        <img src="@/assets/img/sidebar/tool.svg" alt="">
-      </div>
-      <div class="icons__item">
+      </li>
+      <router-link to="/dashboard/information" tag="li" class="icons__item" active-class="icons__item icons-active">
+      <img src="@/assets/img/sidebar/tool.svg" alt="">
+      </router-link>
+      <li class="icons__item">
         <img src="@/assets/img/sidebar/calendar.svg" alt="">
-      </div>
-    </div>
+      </li>
+    </ul>
 
   </div>
 </template>
